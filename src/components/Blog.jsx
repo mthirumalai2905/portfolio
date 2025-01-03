5b6b3df6b4724436253e9d46d377c788c5a73a82
Thirumalai <thiru@thiru.com>
2025-01-03 11:49:42 +0530

    Added

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -12,26 +12,31 @@ const Blog = () => {
       title: "Vector Databases",
       brief: "A vector database is a type of database optimized for storing, indexing, and querying high-dimensional vectors. these vectors often represent data in...",
       coverImage: img1,
+      link: "https://tthirruu.hashnode.dev/vector-databases", // Add the link here
     },
     {
       title: "Scaling Whatsapp",
       brief: "Ever wondered how your WhatsApp messages reach instantly, no matter where you are? Scaling chat systems ensures every message gets through, even with...",
       coverImage: img2,
+      link: "https://tthirruu.hashnode.dev/scaling-whatsapp", // Add the link here
     },
     {
       title: "Scaling Databases",
-      brief: "Clustering Clustering involves connecting multiple servers to work together as a single unit to imprve database performance, scalability, and fault...",
+      brief: "Clustering Clustering involves connecting multiple servers to work together as a single unit to improve database performance, scalability, and fault...",
       coverImage: img3,
+      link: "https://tthirruu.hashnode.dev/scaling-databases", // Add the link here
     },
     {
       title: "Why do Databases use B Trees",
-      brief: "when we talk about storage RAM and HardDisks come to the picture Let’s understand there fucntions and how they differ from each...",
+      brief: "When we talk about storage, RAM and Hard Disks come into the picture. Let’s understand their functions and how they differ from each...",
       coverImage: img4,
+      link: "https://tthirruu.hashnode.dev/why-do-databases-use-b-trees", // Add the link here
     },
     {
       title: "System Design of Google Docs",
-      brief: "Let’s do a system design of collaborative Editing and scale them Flowchart Functional Requirements Tools for Collaborative Editing Non-Functional...",
+      brief: "Let’s do a system design of collaborative Editing and scale them. Flowchart, Functional Requirements, Tools for Collaborative Editing, Non-Functional...",
       coverImage: img5,
+      link: "https://tthirruu.hashnode.dev/system-design-of-google-docs", // Add the link here
     },
   ];
 
@@ -45,7 +50,7 @@ const Blog = () => {
             <div className="blog-card-content">
               <h3>{blog.title}</h3>
               <p>{blog.brief}</p>
-              <a href="#" target="_blank" rel="noopener noreferrer">
+              <a href={blog.link} target="_blank" rel="noopener noreferrer">
                 Read More
               </a>
             </div>
